88fd5a638e102f61695ff5591cab12f146154aeb
Suraz Subedi <surazsubedi28@gmail.com>
2025-03-20 07:41:24 +0545

    update user data

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,25 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import React from "react";
+
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+    const user = await currentUser();
+
+    if (user) {
+        await prisma.user.upsert({
+            where: { clerkUserId: user.id },
+            update: {},
+            create: {
+                name: user.fullName as string,
+                clerkUserId: user.id,
+                email: user.emailAddresses[0].emailAddress,
+                imageUrl: user.imageUrl,
+            },
+        });
+    }
+
+    return <div>{children}</div>; // Always return children
+};
+
+export default Layout;
