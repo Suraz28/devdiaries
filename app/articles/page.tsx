66fa744744bb581e16620fa8682620bd28176418
Suraz Subedi <surazsubedi28@gmail.com>
2025-03-21 09:16:57 +0545

    update

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,11 +1,11 @@
 import ArticleSearchInput from "@/components/articles/article-search-input";
 import { Button } from "@/components/ui/button";
 import React, { Suspense } from "react";
-import { Card } from "@/components/ui/card";
 import Link from "next/link";
 import AllArticlesPage from "../../components/articles/all-articles-page";
 import { fetchArticleByQuery } from "@/lib/query/fetch-articles";
-import { Skeleton } from "@/components/ui/skeleton";
+import { AllArticlesPageSkeleton } from "@/lib/all-articles-page-skeleton";
+
 
 type SearchPageProps = {
   searchParams: { search?: string; page?: string };
@@ -37,7 +37,7 @@ export default async function Page({ searchParams }: SearchPageProps) {
         </div>
         
         {/* All article page */}
-        <Suspense fallback={<AllArticlesPageSkeleton />}>
+        <Suspense fallback={<AllArticlesPageSkeleton/>}>
           <AllArticlesPage articles={articles} />
         </Suspense>
 
@@ -71,45 +71,6 @@ export default async function Page({ searchParams }: SearchPageProps) {
           </Link>
         </div>
       </main>
-    </div>
-  );
-}
-
-// Skeleton Loader for Articles
-export function AllArticlesPageSkeleton() {
-  return (
-    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-      {Array.from({ length: 3 }).map((_, index) => (
-        <Card
-          key={index}
-          className="group relative overflow-hidden transition-all hover:shadow-lg"
-        >
-          <div className="p-6">
-            {/* Article Image Skeleton */}
-            <Skeleton className="mb-4 h-48 w-full rounded-xl bg-gradient-to-br from-purple-100/50 to-blue-100/50 dark:from-purple-900/20 dark:to-blue-900/20" />
-
-            {/* Article Title Skeleton */}
-            <Skeleton className="h-6 w-3/4 rounded-lg" />
-
-            {/* Article Category Skeleton */}
-            <Skeleton className="mt-2 h-4 w-1/2 rounded-lg" />
-
-            {/* Author & Metadata Skeleton */}
-            <div className="mt-6 flex items-center justify-between">
-              <div className="flex items-center gap-3">
-                {/* Author Avatar Skeleton */}
-                <Skeleton className="h-8 w-8 rounded-full" />
-
-                {/* Author Name Skeleton */}
-                <Skeleton className="h-4 w-20 rounded-lg " />
-              </div>
-
-              {/* Date Skeleton */}
-              <Skeleton className="h-4 w-24 rounded-lg " />
-            </div>
-          </div>
-        </Card>
-      ))}
     </div>
   );
 }
