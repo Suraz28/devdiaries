4a133009eab957aa7bd838123a26373b7e065953
Suraz Subedi <surazsubedi28@gmail.com>
2025-03-21 15:32:57 +0545

    update

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,33 +1,89 @@
-import ArticleDetailPage from "@/components/articles/article-detail-page";
-import { prisma } from "@/lib/prisma";
-import React from "react";
+import ArticleSearchInput from "@/components/articles/article-search-input";
+import { Button } from "@/components/ui/button";
+import React, { Suspense } from "react";
+import { fetchArticleByQuery } from "@/lib/query/fetch-articles";
+import Link from "next/link";
+import { AllArticlesPageSkeleton } from "@/lib/all-articles-page-skeleton";
+import AllArticlesPage from "@/components/articles/all-articles-page";
 
-type ArticleDetailPageProps = {
-  params: Promise<{ id: string }>;
+type SearchPageProps = {
+  searchParams: { search?: string; page?: string };
 };
 
-const page: React.FC<ArticleDetailPageProps> = async ({ params }) => {
-  const id = (await params).id;
-  const article = await prisma.articles.findUnique({
-    where: {
-      id,
-    },
-    include: {
-      author: {
-        select: {
-          name: true,
-          email: true,
-          imageUrl: true,
-        },
-      },
-    },
-  });
-  if (!article) {
-    return <h1>Article not found.</h1>;
-  }
+const ITEMS_PER_PAGE = 3; // Number of items per page
+
+const page: React.FC<SearchPageProps> = async ({ searchParams }) => {
+  const searchText = searchParams.search || "";
+  const currentPage = Number(searchParams.page) || 1;
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE;
+  const take = ITEMS_PER_PAGE;
+
+  const { articles, total } = await fetchArticleByQuery(searchText, skip, take);
+  const totalPages = Math.ceil(total / ITEMS_PER_PAGE);
+ 
+
   return (
-    <div>
-      <ArticleDetailPage article={article} /> 
+    <div className="min-h-screen bg-background">
+      <main className="container mx-auto px-4 py-12 sm:px-6 lg:px-8">
+        {/* Page Header */}
+        <div className="mb-12 space-y-6 text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
+            All Articles
+          </h1>
+          {/* Search Bar */}
+          <Suspense>
+            <ArticleSearchInput />
+          </Suspense>
+        </div>
+        {/* All article page  */}
+        <Suspense fallback={<AllArticlesPageSkeleton/>}>
+        <AllArticlesPage articles={articles} />
+        </Suspense>
+        {/* <AllArticlesPageSkeleton/> */}
+        {/* Pagination */}
+        <div className="mt-12 flex justify-center gap-2">
+          {/* Prev Button */}
+          <Link
+            href={`?search=${searchText}&page=${currentPage - 1}`}
+            passHref
+          >
+            <Button variant="ghost" size="sm" disabled={currentPage === 1}>
+              ← Prev
+            </Button>
+          </Link>
+
+          {/* Page Numbers */}
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <Link
+              key={index}
+              href={`?search=${searchText}&page=${index + 1}`}
+              passHref
+            >
+              <Button
+                variant={`${currentPage === index + 1 ? 'destructive' : 'ghost'}`}
+                size="sm"
+                disabled={currentPage === index + 1}
+              >
+                {index + 1}
+              </Button>
+            </Link>
+          ))}
+
+          {/* Next Button */}
+          <Link
+            href={`?search=${searchText}&page=${currentPage + 1}`}
+            passHref
+          >
+            <Button
+              variant="ghost"
+              size="sm"
+              disabled={currentPage === totalPages}
+            >
+              Next →
+            </Button>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 };
