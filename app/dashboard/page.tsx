224637cbc5d503746a016e48829e772bf8d44f90
Suraz Subedi <surazsubedi28@gmail.com>
2025-03-21 08:32:12 +0545

    blogdashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+import { BlogDashboard } from '@/components/dashboard/blog-dashboard';
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+    <BlogDashboard/>
+    </div>
+  )
+}
+
+export default Dashboard;
